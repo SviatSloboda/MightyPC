64e08684071c91072ce236f0c054ee5662e2b518
Sviat <slsvyatko@gmail.com>
2024-02-07 12:27:41 +0100

    fixed img imports

--- frontend/src/components/hardware/HardwarePage.tsx
+++ frontend/src/components/hardware/HardwarePage.tsx
@@ -1,14 +1,22 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import cpu from "../../assets/cpu.png";
+import gpu from "../../assets/gpu.png";
+import motherboard from "../../assets/motherboard.png";
+import hdd from "../../assets/hdd.png";
+import ram from "../../assets/ram.png";
+import psu from "../../assets/psu.png";
+import ssd from "../../assets/ssd.png";
+import pcCase from "../../assets/pcCase.png";
 
 const hardwareItems = [
-    { name: 'CPU', image: 'cpu.png', path: '/hardware/cpu' },
-    { name: 'GPU', image: 'gpu.png', path: '/hardware/gpu' },
-    { name: 'Motherboard', image: 'motherboard.png', path: '/hardware/motherboard' },
-    { name: 'RAM', image: 'ram.png', path: '/hardware/ram' },
-    { name: 'SSD', image: 'ssd.png', path: '/hardware/ssd' },
-    { name: 'HDD', image: 'hdd.png', path: '/hardware/hdd' },
-    { name: 'Power Supply', image: 'psu.png', path: '/hardware/power-supply' },
-    { name: 'PC Case', image: 'pcCase.png', path: '/hardware/pc-case' },
+    {name: 'CPU', image: cpu, path: '/hardware/cpu'},
+    {name: 'GPU', image: gpu, path: '/hardware/gpu'},
+    {name: 'Motherboard', image: motherboard, path: '/hardware/motherboard'},
+    {name: 'RAM', image: ram, path: '/hardware/ram'},
+    {name: 'SSD', image: ssd, path: '/hardware/ssd'},
+    {name: 'HDD', image: hdd, path: '/hardware/hdd'},
+    {name: 'Power Supply', image: psu, path: '/hardware/power-supply'},
+    {name: 'PC Case', image: pcCase, path: '/hardware/pc-case'},
 ];
 
 export default function HardwarePage() {
@@ -16,7 +24,7 @@ export default function HardwarePage() {
         <section className="hardware-section">
             {hardwareItems.map((item) => (
                 <Link to={item.path} className="hardware-item" key={item.name}>
-                    <img src={`src/assets/${item.image}`} alt={`${item.name}`} className="hardware-item__image"/>
+                    <img src={`${item.image}`} alt={`${item.name}`} className="hardware-item__image"/>
                     <span className="hardware-item__name">{item.name}</span>
                 </Link>
             ))}
