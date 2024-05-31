8cbe8d2efb8a40ac607e9a7df7603aa748f55837
Sviat <slsvyatko@gmail.com>
2024-05-31 18:15:27 +0200

    added to cors configurer domain of deployed site

--- backend/src/main/java/de/mightypc/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/mightypc/backend/security/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://mightypc.onrender.com/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
